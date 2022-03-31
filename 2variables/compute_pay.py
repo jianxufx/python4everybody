968448ac5c7a746af6ecc5eff25f303792ef5fee
jianxufx <terrywang361@gmail.com>
2022-03-31 15:00:27 +0800

    Update compute_pay.py

--- 2variables/compute_pay.py
+++ 2variables/compute_pay.py
@@ -2,4 +2,6 @@ hours=input('please enter your hours:')
 rate=input('please enter your rate:')
 
 #note:the return value of input is a string type
-print('your Pay is :',int(hours)*float(rate))
+pay=int(hours)*float(rate)
+
+print('your Pay is :',round(pay,2))
