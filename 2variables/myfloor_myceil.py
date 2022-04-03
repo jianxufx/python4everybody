1da89c71467283a0a79168daaf4e6b38582b7790
jianxufx <terrywang361@gmail.com>
2022-04-03 22:37:13 +0800

    floor() ceil()
    write my own floor() and ceil() based on the built-in function int()

---
+++ 2variables/myfloor_myceil.py
@@ -0,0 +1,23 @@
+#
+def myfloor(x):
+    # if x a type a x.0
+    if(float(x)==int(x)):
+        return x
+    # if x is not a type of x.0
+    if x>0:
+        return int(x)
+    elif x<0:
+        return int(x)-1
+    else:
+        return 0
+def myceil(x):
+    # if x a type a x.0
+    if(float(x)==int(x)):
+            return x
+    # if x is not a type of x.0
+    if x>0:
+        return int(x)+1
+    elif x<0:
+        return int(x)
+    else:
+        return 0
