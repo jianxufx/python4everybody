39e536f546aaec2715e0f14b028c1e698b4b4d84
jianxufx <terrywang361@gmail.com>
2022-04-05 23:05:05 +0800

    Create prompt_max_min.py

---
+++ 5loop_iteration/prompt_max_min.py
@@ -0,0 +1,27 @@
+max=None
+min=None
+while True:
+    num=input('please enter a number:')
+
+    if num=='done':
+        break
+    try:
+        num=float(num)
+    except:
+        print('only numbers are allowed')
+        continue
+
+    #the first input of user
+    if max==None:
+        max=num
+    else:
+        if num>max:
+            max=num
+
+    if min==None:
+        min=num
+    else:
+        if num<min:
+            min=num
+
+print(max,min)
