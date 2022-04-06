52d5cf4eead43db7c2707f07eca4865701f1b28b
jianxufx <terrywang361@gmail.com>
2022-04-06 11:08:30 +0800

    Update travel_string_from_backwards.py

--- 6strings/travel_string_from_backwards.py
+++ 6strings/travel_string_from_backwards.py
@@ -1,7 +1,13 @@
 a='congratulations'
-
+'''
 index =-1
 
 while index>=-len(a):
+    print(a[index])
+    index=index-1
+'''
+index=len(a)-1
+
+while index>=0:
     print(a[index])
     index=index-1
