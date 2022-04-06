29393787719acbb36afae44940253e493aeb1a25
jianxufx <terrywang361@gmail.com>
2022-04-06 17:39:53 +0800

    Update parsing.py

--- 6strings/parsing.py
+++ 6strings/parsing.py
@@ -1,7 +1,11 @@
 sender='From stephen.marquard@ uct.ac.za Sat Jan  5 09:14:16 2008'
-
+'''
 start=sender.find('@')
 end=sender.find('S')
 
 domain=sender[start+1:end]
 print(domain)
+'''
+
+a=sender.split()
+print(a[2])
