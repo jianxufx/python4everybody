0dd4910cf82e0d67c8d5259f89ed33dad3ef6df5
jianxufx <terrywang361@gmail.com>
2022-04-10 22:45:59 +0800

    Update temp.py

--- 9dictionary/temp.py
+++ 9dictionary/temp.py
@@ -1,3 +1,21 @@
-a=dict()
+'''dict_test = { 'chuck' : 1 , 'annie' : 42, 'jan': 100}
 
-a[]
+li=dict_test.values()
+
+
+for i in li:
+    print(i)
+'''
+
+
+a=dict()
+a['height']=155
+a['name']='hanmeimei'
+a[1]=10
+a[3.14]=27149
+print(a)
+
+
+b={1:2,3:'nihao'}
+
+print(b[1])
