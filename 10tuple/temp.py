87b69b1a47c2827d21b737f7586165f79237d9ff
jianxufx <terrywang361@gmail.com>
2022-04-16 17:00:36 +0800

    Update temp.py

--- 10tuple/temp.py
+++ 10tuple/temp.py
@@ -1,16 +1,8 @@
-txt = 'but soft what light in yonder window breaks'
-
-a=list()
-
-for word in txt.split():
-    a.append([len(word),word])
-
-a.sort(reverse=True)
-
-'''
-a=list('hello hanmeimei jim lilei')
-
-for i,k in a.
+a={}
+b=(1,3.14,'lilei')
 
 
-'''
+a[b]=18845687878
+
+
+print(type(a))
