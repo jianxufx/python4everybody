a05b30042134e90b6b20ed56217c4727e48cadc6
jianxufx <terrywang361@gmail.com>
2022-04-17 13:43:13 +0800

    Update exercise2.py

--- 10tuple/exercise2.py
+++ 10tuple/exercise2.py
@@ -20,4 +20,55 @@ Enter a file name: mbox-short.txt
 18 1
 19 1
 
+sample line :From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
+
+
 '''
+timecount=dict()
+#read file with r mode
+try:
+    filehandle =open('mbox-short.txt')
+except:
+    print('file not exist!')
+    a=input('Please any key to continue:')
+    exit()
+
+#read each line through the file
+
+for line in filehandle :
+
+    #we get each line here
+
+    #we care the line starts with "From "
+
+    if not line.startswith('From '):
+        continue
+
+    #clearn the \n
+
+    line=line.strip()
+
+    #split
+
+    wordslist=line.split()
+
+    #wordslist[-2] is what we want
+
+    #split the wordslist[-2] with :
+
+    timelist=wordslist[-2].split(':')
+
+    #build the dictionary
+    #timelist[0] is what we want
+
+    timecount[timelist[0]]=timecount.get(timelist[0],0)+1
+
+li=[]
+
+for i ,k in timecount.items():
+    li.append((i,k))
+
+li.sort()
+
+for i ,k in li:
+    print(i,k)
